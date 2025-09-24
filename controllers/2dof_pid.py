03945c4a64b9c979ae048c4300744e738e04ae59
Siddhant Baroth <siddhant.official8@gmail.com>
2025-09-23 22:55:16 -0400

    Functional 2DOF_PID Controller

--- controllers/2dof_pid.py
+++ controllers/2dof_pid.py
@@ -17,39 +17,30 @@ class Controller(BaseController):
     self.i = 0.1
     self.d = 0.01
 
-    self.b = 0.0
+    self.b = 1.0
+    self.c = 0.5
+    self.alpha = 0.8
 
     self.error_integral = 0
-    self.prev_error = 0
+    self.derivative = 0.0
+    self.prev_error_d = 0.0
     self.dt = getattr(self, "dt", 0.1)
 
   def update(self, target_lataccel, current_lataccel, state, future_plan):
 
-    # Accounting for Road Roll
-    # if future_plan.roll_lataccel:
-    #   print(future_plan.roll_lataccel[0])
-    #   upcoming_roll = future_plan.roll_lataccel[0]
-    #   roll_weight = 0.8
-    #   possible_target_lataccel = target_lataccel - upcoming_roll*roll_weight
-    #   error = possible_target_lataccel - current_lataccel
-    # else:
-    #   error = (target_lataccel - current_lataccel)
-
-    # Using 2DOF PID
-    # u[k] = Kp*ep[k] + I[k] + u_d[k]
-
-    # Proportional Terms
+    # Proportional Error
     error_p = (self.b * target_lataccel) - current_lataccel
 
-    # Integration Terms:  I[k] = I[k-1] + K_i*e_i[k]
+    # Integral Error:  I[k] = I[k-1] + K_i*e_i[k]
     error_i = target_lataccel - current_lataccel
     self.error_integral += self.i * error_i
 
     # Derivative Error:
     error_d = (self.c * target_lataccel) - current_lataccel
+    error_diff = error_d - self.prev_error_d
+    self.prev_error_d = error_d
+    self.derivative = self.alpha * self.derivative + (1 - self.alpha) * error_diff
 
-    error = (target_lataccel - current_lataccel)
+    u = self.p * error_p + self.error_integral + self.d * self.derivative
 
-    error_diff = error - self.prev_error
-    self.prev_error = error
-    return self.p * error + self.i * self.error_integral + (self.d * error_diff)
+    return float(u)
