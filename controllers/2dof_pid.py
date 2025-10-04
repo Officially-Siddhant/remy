373517add67cf993d70a15d5d661554bec46714b
Siddhant Baroth <siddhant.official8@gmail.com>
2025-10-04 16:23:58 -0400

    2dof PID is now at 65! Removing windup helped

--- controllers/2dof_pid.py
+++ controllers/2dof_pid.py
@@ -7,55 +7,68 @@ class Controller(BaseController):
   A simple PID controller
   """
   def __init__(self,):
+  # def __init__(self, alpha, p, i, d, b, c,dt):
 
-    self.p = 0.15
-    self.i = 0.01
-    self.d = 0.1
+    self.p = 0.20
+    self.i = 0.05
+    self.d = 0.20
 
-    self.b = 2.0
-    self.c = 0.11
-    self.alpha = 0.9
+    self.b = 1.5 # higher b = higher reference signal
+    self.c = 0.10 # lower c values - higher the derivative operates on y (measurement)
+    self.alpha = 0.98
+
+    '''
+    self.p = p
+    self.i = i
+    self.d = d
+
+    self.b = b # higher b = higher reference signal
+    self.c = c # lower c values - higher the derivative operates on y (measurement)
+    self.alpha = alpha
+    '''
 
     self.error_integral = 0
     self.derivative = 0.0
     self.prev_error_d = None
 
-    self.k_roll = 0.3
-    # self.alpha_roll = 0.90
-    self.alpha_roll = 0.5
-    self.roll_f = 0.0
-    self.roll_inited = False
+    self.k_roll = 0.20
     self.roll_N = 5
 
-    self.dt = getattr(self, "dt", 0.1)
+    # self.dt = getattr(self, "dt", 0.1)
+    self.dt = 1.0
+    self.steer_factor = 12.5
+    self.minimum_v = 20.0
+    self.N = 5
 
   def update(self, target_lataccel, current_lataccel, state, future_plan):
+    self.step = getattr(self, "step", 0) + 1
+    fade = min(self.step / 100.0, 1.0)
+
+    # gentlest on I, moderate on P/D
+    Kp_eff = self.p * fade
+    Ki_eff = self.i * (fade ** 3)
+    Kd_eff = self.d * fade
+
 
     # Feed-forward terms
-    rolls = getattr(future_plan, "roll_lataccel", []) or []
-    if rolls:
-      # average first N points to reduce noise
-      r0 = float(np.mean(rolls[:min(self.roll_N, len(rolls))]))
-      if not self.roll_inited:
-        # roll_f = mean(rolls)
-        self.roll_f, self.roll_inited = r0, True
-      else:
-        # roll_f = α*roll_f + (1-α) * mean(rolls)
-        self.roll_f = self.alpha_roll * self.roll_f + (1 - self.alpha_roll) * r0
-    #
-    # Feed-forward control term
-    u_ff = -self.k_roll * self.roll_f
+    rolls = [getattr(state, "roll_lataccel", 0.0)] + (getattr(future_plan, "roll_lataccel", []) or [])
+    u_ff = -self.k_roll * np.average(rolls[:self.roll_N])
 
+    future = future_plan.lataccel[:max(0, self.N - 1)]
+    targets = [float(target_lataccel)] + [float(x) for x in future]
+    weights = np.array([4, 3, 2, 2, 2, 1][:len(targets)], dtype=float)
+    weights /= weights.sum()
+    target_smooth = float(np.dot(weights, targets))
 
     # Proportional Error
-    error_p = (self.b * target_lataccel) - current_lataccel
+    error_p = (self.b * target_smooth) - current_lataccel
 
     # Integral Error
-    error_i = target_lataccel - current_lataccel
-    self.error_integral += self.i*error_i
+    error_i = target_smooth - current_lataccel
+    self.error_integral += Ki_eff*error_i
 
     # Derivative Error
-    error_d = (self.c * target_lataccel) - current_lataccel
+    error_d = (self.c * target_smooth) - current_lataccel
     if self.prev_error_d is None:
       error_diff = 0.0
     else:
@@ -63,6 +76,14 @@ class Controller(BaseController):
     self.prev_error_d = error_d
     self.derivative = self.alpha * self.derivative + (1 - self.alpha) * error_diff
 
-    u = self.p * error_p + self.error_integral + self.d * self.derivative
 
-    return float(u + u_ff)
+    steer_lataccel_target = (target_smooth - state.roll_lataccel)
+    u_steer = steer_lataccel_target * (self.steer_factor / max(self.minimum_v, state.v_ego))
+    u_steer = u_steer / (1.0 + np.abs(u_steer))
+
+    u = Kp_eff * error_p + self.error_integral + Kd_eff * self.derivative
+
+
+    u_tot = u + u_steer + u_ff
+
+    return u_tot
